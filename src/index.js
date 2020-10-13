41182667c4e2b351bac24b975c0a67e93de0fe75
Ekaterina Pokras <eapeip@mail.ru>
2020-10-13 13:35:55 +0500

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let str1 = [];
+  let str2 = str.split(', ').join('');
+
+  while (str2 !== str1) {
+str1 = str2;
+  
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    str2 = str2.split(bracketsConfig[i][0] + bracketsConfig[i][1]).join('');
+  }
+
+  }
+  
+let res = (str2 === '') ? true : false;
+  return res;
 }
